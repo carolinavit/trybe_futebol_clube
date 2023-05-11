3b5b6d44ca40bdd09a56d143c583adef6493945e
Vitória <vitcs11@gmail.com>
2023-05-11 11:55:57 -0300

    feat(src/backend): Cria model e migration para a tabela de matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,58 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+import Team from './TeamModel';
+
+class Match extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeamId: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamId: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'matches',
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+Team.hasMany(Match, { foreignKey: 'homeTeamId' });
+Match.belongsTo(Team, { foreignKey: 'homeTeamId' });
+
+Team.hasMany(Match, { foreignKey: 'awayTeamId' });
+Match.belongsTo(Team, { foreignKey: 'awayTeamId' });
+
+export default Match;
